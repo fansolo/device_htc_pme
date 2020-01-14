96b08ab8447e9346167f24f247bb0ab2c78cd0a1
fansolo <smontero@gmail.com>
2020-01-14 00:20:29 -0500

    pme:rename aosp related fileutils

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -8,9 +8,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
